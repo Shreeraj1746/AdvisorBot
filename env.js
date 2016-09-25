ea12023a3458ab520d5ff9d188aaf6e6b37ebb80
Shreeraj1746 <shreeraj.dabholkar@sjsu.edu>
2016-09-25 12:41:15 -0700

    Delete env.js

--- env.js
+++
@@ -1,4 +0,0 @@
-process.env['clientId'] = "3165132061.76076175159"
-process.env['clientSecret'] = "1e57ca18623ec6fe938a8c096310e7f7"
-process.env['port'] = "4114"
-process.env['wit'] = "6UMUVTNVJ2KHALBI3DHE563KHWGDSF3I"
